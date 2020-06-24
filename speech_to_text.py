213b96e2b6fc780b92bbf4df4b22c215ad8c25c1
tsc10 <thirumaran.sc@aexp.com>
2020-06-24 16:01:24 -0700

    variable updates

--- speech_to_text.py
+++ speech_to_text.py
@@ -14,39 +14,39 @@ def record_voice_stream(stream_file_name):
     frames_count = 1024  # Number of frames per buffer
     record_seconds = 5
 
-    p = pyaudio.PyAudio()
+    stream = pyaudio.PyAudio()
 
-    stream = p.open(format=stream_format,
-                    channels=no_of_channels,
-                    rate=sampling_rate,
-                    input=True,
-                    frames_per_buffer=frames_count)  # number of frames
+    stream_data = stream.open(format=stream_format,
+                              channels=no_of_channels,
+                              rate=sampling_rate,
+                              input=True,
+                              frames_per_buffer=frames_count)
     print("Please speak to record your voice")
-    frames = [stream.read(frames_count) for i in range(0, int(sampling_rate / frames_count * record_seconds))]
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+    frames = [stream_data.read(frames_count) for i in range(0, int(sampling_rate / frames_count * record_seconds))]
+    stream_data.stop_stream()
+    stream_data.close()
+    stream.terminate()
 
-    wf = wave.open(stream_file_name, 'wb')
-    wf.setnchannels(no_of_channels)
-    wf.setsampwidth(p.get_sample_size(stream_format))
-    wf.setframerate(sampling_rate)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+    wave_file = wave.open(stream_file_name, 'wb')
+    wave_file.setnchannels(no_of_channels)
+    wave_file.setsampwidth(stream.get_sample_size(stream_format))
+    wave_file.setframerate(sampling_rate)
+    wave_file.writeframes(b''.join(frames))
+    wave_file.close()
     print(f'Voice stream file {stream_file_name} is created')
 
 
 def predict_speech_to_text(stream_file):
     # Initialize the model
-    ds = Model(MODEL_PATH)
+    speech_model = Model(MODEL_PATH)
 
     # Enable language scorer to improve the accuracy
-    ds.enableExternalScorer(SCORER_PATH)
+    speech_model.enableExternalScorer(SCORER_PATH)
     # You can play with setting the model Beam Width, Scorer language model weight and word insertion weight
 
     # Use scipy to covert wav file into numpy array
-    fs, audio = wav.read(stream_file)
-    return ds.stt(audio)
+    _, audio = wav.read(stream_file)
+    return speech_model.stt(audio)
 
 
 if __name__ == '__main__':
@@ -54,3 +54,4 @@ if __name__ == '__main__':
     record_voice_stream(output_stream_file)
     print('Start of text prediction')
     print(f'DeepSpeech predicted text: {predict_speech_to_text(output_stream_file)}')
+
